e8d81e8c2b57963e9c6d7c80875d2b212afe9c68
Dieterich Lawson <dieterich.lawson@gmail.com>
2015-07-29 13:47:14 -0700

    First commit of pack script

---
+++ pack.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+RECIPIENT_PUBLIC_KEY=$1
+SENDER_PRIVATE_KEY=$2
+PLAINTEXT=$3
+# add secret and their public key to tar file
+cp $PLAINTEXT plaintext
+cp $RECIPIENT_PUBLIC_KEY to.pem
+tar -c plaintext to.pem > adressed_plaintext.tar
+rm to.pem plaintext
+# sign digest of tar
+openssl dgst -sha256 -sign $SENDER_PRIVATE_KEY -out signature adressed_plaintext.tar
+# add plaintext and signature to new tar file
+tar -c adressed_plaintext.tar signature > signed_plaintext.tar
+# create random password for aes key
+openssl rand -base64 32 > aes_pwd
+# encrypt tarfile with aes using aes_pwd
+cat aes_pwd | openssl aes-256-cbc -a -base64 -salt -in signed_plaintext.tar -pass stdin -out sign
+# encrypt aes password using recipient public key
+openssl rsautl -encrypt -inkey $1 -pubin -in aes_pwd -out aes_pwd.enc
+# add encrypted aes password and encrypted tar file to a new tarfile
+tar -c signed_plaintext.enc aes_pwd.enc > encrypted.tar
+cat plaintext
+rm plaintext adressed_plaintext.tar signature signed_plaintext.tar signed_plaintext.enc \
